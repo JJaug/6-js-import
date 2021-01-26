d13d83dbe021ce36ac6e8909f4f70198c11a3e75
Jonas <jonas.jaugas@gmail.com>
2021-01-26 14:55:54 +0200

    import-export

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import {sumAll} from './components/sumAll.js';
+import {duomenys1} from './data/duomenys-1.js';
+import {duomenys2} from './data/duomenys-2.js';
+
+sumAll(duomenys1);
+sumAll(duomenys2);
